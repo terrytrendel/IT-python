20f4dcda8bf9340ff9a898c5b249077a0370d686
Terry Trendel <terry.trendel@gmail.com>
2019-09-20 14:12:38 -0400

    Gets basic journal working

--- ROCK_PAPER_SICSSORS.py
+++ ROCK_PAPER_SICSSORS.py
@@ -1,56 +1,62 @@
 import random
+yes = "yes"
+while yes == "yes":
+        player_score = 0
+        computer_score = 0
+        print("============================")
+        print("   ROCK, PAPER, SCISSORS    ")
+        print("      By.Terry Trendel      ")
+        print("      and Adam Bulk         ")
+        print("============================")
 
-player_score = 0
-computer_score = 0
-print("============================")
-print("   ROCK, PAPER, SCISSORS    ")
-print("      By.Terry Trendel      ")
-print("      and Adam Bulk         ")
-print("============================")
+        t = ["", "rock", "paper", "scissors"]
 
-t = ["", "rock", "paper", "scissors"]
+        print("We are going to play Rock, Paper, Scissors. The first to win two out of three rounds is the winner.")
+        input("")
+        while player_score < 2:
+            print(f"{player_score} - {computer_score}")
+            print("")
+            if computer_score == 2:
+                print("You lose")
+                break
+            print("1. Rock")
+            print("2. Paper")
+            print("3. Scissors")
+            player_choice = int(input("What do you choose? "))
+            computer_choice = random.randint(1, 3)
+            if player_choice == computer_choice:
+                print("tie")
+            elif player_choice == 1:
+                if computer_choice == 2:
+                    print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
+                    computer_score = computer_score + 1
+                else:
+                    print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
+                    player_score = player_score + 1
+            elif player_choice == 2:
+                if computer_choice == 3:
+                    print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
+                    computer_score = computer_score + 1
+                else:
+                    print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
+                    player_score = player_score + 1
+            elif player_choice == 3:
+                if computer_choice == 1:
+                    print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
+                    computer_score = computer_score + 1
+                else:
+                    print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
+                    player_score = player_score + 1
+        if player_score == 2:
+            print("You win")
+        play_again = "no"
+        while play_again == "no":
+            play_again = input("Play again? ")
+            if play_again == "yes":
+                print("ok")
+                break
+            else:
+                play_again == "no"
+                print("say yes")
 
-print("We are going to play Rock, Paper, Scissors. The first to win two out of three rounds is the winner.")
-input("")
-while player_score < 2:
-    print(f"{player_score} - {computer_score}")
-    print("")
-    if computer_score == 2:
-        print("You lose")
-        break
-    print("1. Rock")
-    print("2. Paper")
-    print("3. Scissors")
-    player_choice = int(input("What do you choose? "))
-    computer_choice = random.randint(1, 3)
-    if player_choice == computer_choice:
-        print("tie")
-    elif player_choice == 1:
-        if computer_choice == 2:
-            print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
-            computer_score = computer_score + 1
-        else:
-            print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
-            player_score = player_score + 1
-    elif player_choice == 2:
-        if computer_choice == 3:
-            print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
-            computer_score = computer_score + 1
-        else:
-            print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
-            player_score = player_score + 1
-    elif player_choice == 3:
-        if computer_choice == 1:
-            print(f"you lose! {t[computer_choice]} covers {t[player_choice]}")
-            computer_score = computer_score + 1
-        else:
-            print(f"you win! {t[player_choice]} covers {t[computer_choice]}")
-            player_score = player_score + 1
 
-
-
-
-
-
-if player_score == 2:
-    print("You win")
--- journal.py
+++ journal.py
@@ -1,4 +1,27 @@
-with open("birthday.py") as file:
-    text = file.readlines()
+import os
 
-print(text)
+def get_full_path(name):
+    filename = os.path.join(".","journals", f"{name}.jrn")
+    return filename
+
+def load(name):
+    data = []
+    filename = get_full_path(name)
+    print(f"...... loading from {filename}")
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
+def save(name, data):
+    filename =get_full_path(name)
+    print(f"..... saving to {filename}")
+    with open(filename, "w") as fout:
+        for entry in data:
+            fout.write(entry+"\n")
+
+
+def add_entry(text, data):
+        data.append(text)
